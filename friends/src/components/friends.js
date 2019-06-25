04c0df67a4cec247594de23f9151046b47fd794d
sam-malanchuk <malanchuksam@gmail.com>
2019-06-25 19:45:27 -0400

    created a Friends component to display all the friends found on the server

--- friends/src/components/friends.js
+++ friends/src/components/friends.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-function Friends() {
+function Friends(props) {
     return (
-        <h1>Something</h1>
+        <div>
+            {props.friends.map(friend => {
+                return <p>{friend.name} is my number {friend.id} friend and he is {friend.age} years old. You can reach him at {friend.email}</p>
+            })}
+        </div>
     );
 };
 
