e285ff251000cb9e203d04877d108e6e1f30e838
sam-malanchuk <malanchuksam@gmail.com>
2019-06-25 21:36:08 -0400

    added final styles to friends component and the update/delete buttons

--- friends/src/components/friends.js
+++ friends/src/components/friends.js
@@ -2,18 +2,26 @@ import React from 'react';
 import './Friends.css';
 
 function Friends(props) {
+    function deleteItem() {
+        console.log("item should delete");
+    };
+    function updateItem() {
+        console.log("item should update");
+    };
     return (
         <div className="itemsContainer">
             {props.friends.map(friend => {
                 return (
-                    <div className="friendItem">
-                        <div className="friendName">Name: {friend.name}</div>
-                        <div className="friendID">ID: {friend.id}</div>
-                        <div className="friendAge">Age: {friend.age}</div>
-                        <div className="friendEmail">Email: {friend.email}</div>
+                    <div className="friendItem" key={friend.id}>
+                       <div className="friendContent">
+                            <div className="friendName">{friend.name}</div>
+                            <div className="friendID">ID: {friend.id}</div>
+                            <div className="friendAge">Age: {friend.age}</div>
+                            <div className="friendEmail">{friend.email}</div>
+                        </div>
                         <div className="friendCRUD">
-                            <button className="update">Update</button>
-                            <button className="delete">Delete</button>
+                            <div className="update" onClick={updateItem}>Update</div>
+                            <div className="delete" onClick={deleteItem}>Delete</div>
                         </div>
                     </div>
                 )
